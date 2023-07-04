ec94a31ef994b2ece4f85076f47660f79d52cb47
Avani <avani.jadeja@gmail.com>
2023-07-04 18:05:02 -0500

    Added comment in store.js file in utils

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,29 +1,9 @@
-// import React, { createContext, useContext } from "react";
-// import { useProductReducer } from './reducers'
-
-// const StoreContext = createContext();
-// const { Provider } = StoreContext;
-
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useProductReducer({
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: '',
-//   });
-
-//   return <Provider value={[state, dispatch]} {...props} />;
-// };
-
-// const useStoreContext = () => {
-//   return useContext(StoreContext);
-// };
-
-// export { StoreProvider, useStoreContext };
-
+// import redux
 import { createStore } from "redux";
+// import reducers
 import reducer from "./reducers";
+// create store for reducer
 const store = createStore(reducer);
 
+// export store
 export default store;
