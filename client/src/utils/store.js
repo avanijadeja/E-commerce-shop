412863c55807405b465fc6bea4859ad1e70b9b10
Avani <avani.jadeja@gmail.com>
2023-07-03 19:00:30 -0500

    delete a globalState and added store for redux

---
+++ client/src/utils/store.js
@@ -0,0 +1,31 @@
+// import React, { createContext, useContext } from "react";
+// import { useProductReducer } from './reducers'
+
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
+
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
+
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
+
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
+
+// export { StoreProvider, useStoreContext };
+
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
+const store = createStore(reducer);
+
+export default store;
