64f2a7aecd56029ed20fc14d451b391833e8ea47
Avani <avani.jadeja@gmail.com>
2023-07-04 18:14:44 -0500

    Added comments in Productitem index.js file

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
+// import react-redux
 import { useDispatch, useSelector } from "react-redux";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
+  // create dispatch from useDispatch()
   const dispatch = useDispatch();
+  // create state from useSelector
   const state = useSelector((s) => s);
 
   const { image, name, _id, price, quantity } = item;
