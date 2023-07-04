d501e076749a782e5b69bb7e27374cdd56af04e0
Avani <avani.jadeja@gmail.com>
2023-07-04 18:13:39 -0500

    Added comments in categoryMenu index.js file

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
+// import react-redux
 import { useDispatch, useSelector } from "react-redux";
 import {
   UPDATE_CATEGORIES,
@@ -9,7 +10,9 @@ import { QUERY_CATEGORIES } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
 
 function CategoryMenu() {
+  // create disptach from useDispatch()
   const dispatch = useDispatch();
+  // create state from useSelector()
   const state = useSelector((s) => s);
   const { categories } = state;
 
